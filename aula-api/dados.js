30a1ab9f76b3c1133cc5643517d5f0078f5fa1fd
Devsamuxx <168596767+Devsamuxx@users.noreply.github.com>
2026-03-27 13:44:31 -0300

    Add files via upload

---
+++ aula-api/dados.js
@@ -0,0 +1,100 @@
+const dados = [
+  {
+    titulo: "JoJO", 
+    descricao: "ANIME",
+    link: "https://..."
+  },
+   {
+    titulo: "To you eternity", 
+    descricao: "anime",
+    link: "https://..."
+  },
+   {
+    titulo: "AKAME GA KILL", 
+    descricao: "ANIME",
+    link: "https://..."
+  },
+   {
+    titulo: "CAVALO da absoluta certeza", 
+    descricao: "avalo",
+    link: "https://..."
+  },
+   {
+    titulo: "cavalo da absoluta negação", 
+    descricao: "",
+    link: "https://..."
+  },
+   {
+    titulo: "A ultima das lagrikas alec", 
+    descricao: "",
+    link: "https://..."
+  },
+   {
+    titulo: "Perdão", 
+    descricao: "pasilhas",
+    link: "https://..."
+  },
+   {
+    titulo: "como q coloca izo", 
+    descricao: "",
+    link: "https://..."
+  }, {
+    titulo: "o final de tudo ", 
+    descricao: "",
+    link: "https://..."
+  }, {
+    titulo: "0", 
+    descricao: "l",
+    link: "https://..."
+  },
+   {
+    titulo: "s1000rr", 
+    descricao: "moto carenada da BMW",
+    link: "https://..."
+  },
+   {
+    titulo: "rd 350", 
+    descricao: "famosa VIÚVA-NEGRA",
+    link: "https://..."
+  },
+   {
+    titulo: "H2R", 
+    descricao: "moto mais rapida do mundo",
+    link: "https://..."
+  },
+   {
+    titulo: "titan 99", 
+    descricao: "titan todo mundo conheçe",
+    link: "https://..."
+  },
+   {
+    titulo: "TENERE 700", 
+    descricao: "não tenho oque colocar na descrição",
+    link: "https://..."
+  },
+   {
+    titulo: "AGRALE dakar", 
+    descricao: "moto nacinal ",
+    link: "https://..."
+  },
+   {
+    titulo: "CRF 250", 
+    descricao: "moto de off-road",
+    link: "https://..."
+  },
+   {
+    titulo: "cbr FIRE-BLADE", 
+    descricao: "espada de fogo para os leigos",
+    link: "https://..."
+  },
+   {
+    titulo: "BMW R1200 GS", 
+    descricao: "MILIDUQUE",
+    link: "https://..."
+  },
+   {
+    titulo: "SAHARA 300", 
+    descricao: "XRE so que mudou o nome",
+    link: "https://..."
+  }
+];
